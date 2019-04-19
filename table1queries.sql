e36d26ad550f5173fd633e0b13f7f9f68d123003
Daniel Way <danieldeanway@gmail.com>
2019-04-18 20:37:24 -0500

    Refined union on Q27

--- table1queries.sql
+++ table1queries.sql
@@ -36,31 +36,18 @@ SET @`month` = 5;
 SET @year1 = 2017;
 SET @year2 = 2016;
 
-DROP TEMPORARY TABLE IF EXISTS InfluentialUsers1;
-DROP TEMPORARY TABLE IF EXISTS InfluentialUsers2;
-CREATE TEMPORARY TABLE InfluentialUsers1 ( uname VARCHAR(15) );
-CREATE TEMPORARY TABLE InfluentialUsers2 ( uname VARCHAR(15) );
-
-INSERT INTO InfluentialUsers1
-SELECT 		u.uname
+(SELECT 		u.uname
 FROM 		user u
 INNER JOIN	tweet t ON t.tweeted_by = u.uname
 WHERE		t.`month` = @`month` AND t.`year` = @year1
 GROUP BY	u.uname
 ORDER BY 	SUM(t.rt_count) DESC
-LIMIT 		10;
-
-INSERT INTO InfluentialUsers2
-SELECT 		u.uname
+LIMIT 		10)
+UNION
+(SELECT 		u.uname
 FROM 		user u
 INNER JOIN	tweet t ON t.tweeted_by = u.uname
 WHERE		t.`month` = @`month` AND t.`year` = @year2
 GROUP BY	u.uname
 ORDER BY 	SUM(t.rt_count) DESC
-LIMIT 		10;
-
-SELECT 		*
-FROM		InfluentialUsers1
-UNION
-SELECT		*
-FROM		InfluentialUsers2;
+LIMIT 		10);
